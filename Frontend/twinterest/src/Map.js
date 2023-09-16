78594beaa5c57061ad4bd19ecc5b7046536f3fc0
dourian <dorcwork@gmail.com>
2023-09-16 09:35:39 -0400

    frontend
    Co-authored-by: bryant <bryantzhengg@users.noreply.github.com>

--- Frontend/twinterest/src/Map.js
+++ Frontend/twinterest/src/Map.js
@@ -1,22 +1,67 @@
-import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import React, { useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 
 const containerStyle = {
-  width: "800px",
-  height: "800px",
+  width: "100vw",
+  height: "100vh",
 };
 
 const center = {
-  lng: 43.4723,
-  lat: 80.5449,
+  lat: 43.473176,
+  lng: -80.539849,
+};
+
+const PlacesAutocomplete = ({ setSelected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  return (
+    <form className={`drop_shadow w-full absolute z-10 top-5`}>
+      <label>
+        <input
+          className={`border-[2px] rounded-lg mx-4 py-1 px-4 h-[50px] w-9/12`} // Change w-10/12 to w-full
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={!ready}
+          placeholder={"Where do you want to go?"}
+        ></input>
+      </label>
+      <button
+        className={`text-sm rounded-lg px-4 py-2 drop_shadow bg-black text-white`}
+        type="submit"
+      >
+        {"Go"}
+      </button>
+      <div>
+        {status === "OK" &&
+          data.map(({ place_id, description }) => (
+            <p key={place_id}>{description}</p>
+          ))}
+      </div>
+    </form>
+  );
 };
 
 function MyComponent() {
+  const google = window.google;
+
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyBgx2IEOegL2ILr6cdBzeymb3-6GChTTIc",
+    libraries: ["places"],
   });
 
+  const [selected, setSelected] = useState(null);
   const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
@@ -32,18 +77,30 @@ function MyComponent() {
   }, []);
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-      heading={false}
-      options={{ disableDefaultUI: true, mapId: "10561e5854fbba2e" }}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
+    <>
+      <div className="places-container">
+        <PlacesAutocomplete setSelected={setSelected} />
+      </div>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        defaultZoom={3}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={{ disableDefaultUI: true, mapId: "10561e5854fbba2e" }}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <Marker
+          position={center}
+          icon={{
+            url: "https://firebasestorage.googleapis.com/v0/b/impactful-ring-399204.appspot.com/o/shinguh.png?alt=media&token=1a8cc7a6-7b71-4b65-a649-bcbaf9269abc",
+            scaledSize: new google.maps.Size(100, 100),
+          }}
+          title="shinguh"
+        />
+        <></>
+      </GoogleMap>
+    </>
   ) : (
     <></>
   );
