3c8cc1549dfd2965b2c666f72e9524b74c6552de
ruiichen <frankchen99999@gmail.com>
2023-09-16 17:03:08 -0400

    guh

--- Frontend/twinterest/src/Hover.js
+++ Frontend/twinterest/src/Hover.js
@@ -2,25 +2,42 @@ import React from "react";
 
 const Hover = () => {
   return (
-    <div className="absolute inset-x-0 bottom-0 h-1/6 w-5/6...">
-      <div className="grid grid-cols-7 gap-7">
-        <div>
-          <div className="absolute left-0 top-0 h-1/100 w-1/100 ...">
-          <img
-          src="https://via.placeholder.com/50"
-          alt="Sample Image"
-          />
+    <div className="absolute inset-x-3 bottom-0 w-fit rounded-t-lg ...">
+        <div className="grid grid-cols-7 gap-7" >
+          <div>
+            <div className="absolute left-0 top-0 ...">
+              <div>
+                <img
+                  className="object-cover p-1 h-1/2 w-1/2 ..."
+                  src="https://via.placeholder.com/100"
+                  alt="Sample Image"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        
 
-        <div className="col-span-6">
-          <div class="grid grid-rows-2 grid-flow-col gap-4">
-          <p>Smiggrep</p>
+
+          <div className="col-span-6">
+            <div className="grid grid-rows-2 gap-1">
+              <p>Smiggrep</p>
+              <p>@Smiggrep</p>
+            </div>
+
           </div>
-          
         </div>
-      </div>
+
+
+        <img
+            className="object-cover p-3 h-48 w-96 ..."
+            src="https://via.placeholder.com/1000" />
+
+
+        <div>
+          <div className="grid grid-rows-2 gap-1">
+            <p>Smiggrep</p>
+            <p>@Smiggrep</p>
+          </div>
+        </div>
 
     </div>
   );
