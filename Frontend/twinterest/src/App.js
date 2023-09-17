1989a7caaf6dd194d432c8028c2ee7824082670d
dourian <dorcwork@gmail.com>
2023-09-16 21:12:32 -0400

    Update App.js

--- Frontend/twinterest/src/App.js
+++ Frontend/twinterest/src/App.js
@@ -15,6 +15,7 @@ function App() {
     console.log(file);
   }
   const handleUpload = () => {
+    console.log("here")
     const myHeaders = new Headers();
     myHeaders.append(
       "Authorization",
